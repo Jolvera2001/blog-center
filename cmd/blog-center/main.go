b3ca2d781b0e68e2fbe83c655acb934ecbd1edda
Jolvera2001 <jolvera6@stedwards.edu>
2024-09-23 12:03:20 -0500

    starting new project

---
+++ cmd/blog-center/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	r.Run()
+}
