86b35bf55f6937f9ba068023859bb24f1cff7b40
Jolvera2001 <jolvera6@stedwards.edu>
2024-09-27 10:19:56 -0500

    created user dto

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,24 +2,41 @@ package service
 
 import (
 	"blog-center/internal/domain"
+	"blog-center/internal/dtos"
 )
 
 type UserService struct {
 	UserRepo domain.IUserRepository
 }
 
-func (s *UserService) RegisterUser(name, email, password string) (*domain.User, error) {
+func (s *UserService) RegisterUser(dto dtos.UserDto) (string, error) {
+	newUser := domain.User{
+		Name: dto.Name,
+		Email: dto.Email,
+		Password: dto.Password,
+	}
+
+	err := s.UserRepo.Create(&newUser)
+	if err != nil {
+		return "", err
+	}
+
+	return newUser.ID, nil
+}
+
+func (s *UserService) GetUserProfile(id string) (*domain.User, error) {
+	foundUser, err := s.UserRepo.FindByID(id)
+	if err != nil {
+		return &domain.User{}, err
+	}
+
+	return foundUser, nil
+}
+
+func (s *UserService) UpdateUserProfile(uuid string, dto dtos.UserDto) error {
 
 }
 
-func (s *UserService) GetUserProfile(id uint) (*domain.User, error) {
-
-}
-
-func (s *UserService) UpdateUserProfile(name, email, password string) error {
-
-}
-
-func (s *UserService) DeleteUserAccount(id uint) error {
+func (s *UserService) DeleteUserAccount(uuid string) error {
 
 }
--- internal/service/user_service_interface.go
+++ internal/service/user_service_interface.go
@@ -4,7 +4,7 @@ import "blog-center/internal/domain"
 
 type IUserService interface {
 	RegisterUser(name, email, password string) (*domain.User, error)
-	GetUserProfile(id uint) (*domain.User, error)
+	GetUserProfile(uuid string) (*domain.User, error)
 	UpdateUserProfile(name, email, password string) error
-	DeleteUserAccount(id uint) error
+	DeleteUserAccount(uuid string) error
 }
